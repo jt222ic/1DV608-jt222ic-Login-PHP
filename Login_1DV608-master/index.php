e05572dde23cab7f8606e311948e95b9ced3a21a
jt222ic <jt222ic@student.lnu.segit config --global user.email jt222ic@student.lnu.se>
2015-09-24 09:10:56 +0000

    lade dateview

--- Login_1DV608-master/index.php
+++ Login_1DV608-master/index.php
@@ -1,7 +1,7 @@
 <?php
 
 
-session_start();                            // startar session först
+session_start();                            // asdsastartar session först
 //INCLUDE THE FILES NEEDED...
 require_once('view/LoginView.php');
 require_once('view/DateTimeView.php');
